8187604946d3cf068cbae78668566ebf6e374213
shohjahon-sohibov <sohibov.s@bk.ru>
2023-04-01 06:43:31 +0300

    fix bugs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', "*");
+    next();
+}
+
+app.use(allowCrossDomain);
 app.use(cors())
 app.use(express.json())
 app.use(require('./modules'))
