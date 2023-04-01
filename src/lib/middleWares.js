57dd1e92d00d61992c8705b0d0d115d4ac5a4b77
shohjahon-sohibov <sohibov.s@bk.ru>
2023-04-01 06:58:47 +0300

    fix

--- src/lib/middleWares.js
+++ src/lib/middleWares.js
@@ -1,5 +1,5 @@
 let allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', "https://my-app-chi-inky.vercel.app");
     res.header('Access-Control-Allow-Headers', "*");
     next();
 }
